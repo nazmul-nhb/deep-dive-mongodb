e7a35e5d255441516086d47d56577d3e6e540230
Nazmul Hassan <nazmulnhb007@yahoo.com>
2024-11-10 14:18:38 +0600

    explored basic operators

---
+++ operators.js
@@ -0,0 +1,9 @@
+db.test.find(
+    {
+        gender: "Female",
+        age:
+            { $nin: [18, 20, 24] },
+        interests: { $in: ["Cooking", "Gaming"] },
+    },
+    { name: 1, gender: 1, age: 1, interests: 1 })
+    .sort({ age: 1 })
