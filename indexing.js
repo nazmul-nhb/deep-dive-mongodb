3a9f3775b9df12331a83b431381ca229270b17a7
Nazmul Hassan <nazmulnhb007@yahoo.com>
2024-11-12 18:36:52 +0600

    explored text indexing

--- indexing.js
+++ indexing.js
@@ -1,4 +1,13 @@
 // db.test.find({"_id" : ObjectId("6406ad63fc13ae5a40000066")}).explain("executionStats")
 // db.test.find({"email" : "omirfin2@i2i.jp"}).explain("executionStats")
 
-db.getCollection("massive").createIndex({ email: 1 })
+// indexing on single field
+// db.getCollection("massive").createIndex({ email: 1 })
+
+// drop index
+// db.getCollection("massive").dropIndex({ email: 1 })
+
+// text indexing
+// db.massive.createIndex({ about: 'text' })
+
+db.massive.find({ $text: { $search: "dolor" } })
